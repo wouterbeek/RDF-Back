3155329c10f3bee955ee41b4700f61118b2b361c
Wouter Beek <wouter@triply.cc>
2018-04-15 03:27:48 +0200

    Added example configuration file.

--- run.pl
+++ run.pl
@@ -1,4 +1,5 @@
 :- use_module(library(sw/axiom/rdfs_axioms)).
+:- use_module(library(sw/backend/hdt_backend)).
 :- use_module(library(sw/rdf_back)).
 :- use_module(library(sw/rdf_ent_export)).
 :- use_module(library(sw/rule/rdfs_rules)).
